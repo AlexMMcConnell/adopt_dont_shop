4c37df63f1df6ad4abfefa4802b9a724d51db72c
Alex McConnell <alexmmcconnell@gmail.com>
2021-10-26 20:15:44 -0600

    Test - Add tests for status changing for application

--- spec/features/admin/applications_spec.rb
+++ spec/features/admin/applications_spec.rb
@@ -6,7 +6,9 @@ RSpec.describe 'admin_application' do
     @application = Application.create!(name: 'Bob Smith', street_address: '321 Eve Way', city: 'Aurora', state: 'Colorado', zip_code: '80222', status: "Pending")
     @application2 = Application.create!(name: 'Jane Smith', street_address: '321 Eve Way', city: 'Aurora', state: 'Colorado', zip_code: '80222', status: "Pending")
     @pet = Pet.create!(adoptable: true, age: 7, breed: 'Golden Retriever', name: 'Brownie', shelter_id: shelter1.id)
+    @pet2 = Pet.create!(adoptable: true, age: 7, breed: 'Husky', name: 'Snow', shelter_id: shelter1.id)
     ApplicationPet.create!(application: @application, pet: @pet)
+    ApplicationPet.create!(application: @application, pet: @pet2)
     ApplicationPet.create!(application: @application2, pet: @pet)
   end
   describe 'button to approve pet application' do
@@ -15,7 +17,7 @@ RSpec.describe 'admin_application' do
 
       expect(current_path).to eq("/admin/applications/#{@application.id}")
 
-      click_button("Accept")
+      click_button id: "#{@pet[:name]} accept"
 
       expect(current_path).to eq("/admin/applications/#{@application.id}")
       expect(page).to have_content("Pet accepted for this Application")
@@ -27,21 +29,56 @@ RSpec.describe 'admin_application' do
       visit "/admin/applications/#{@application.id}"
 
       expect(current_path).to eq("/admin/applications/#{@application.id}")
-      click_button("Deny")
+
+      click_button id: "#{@pet.name} deny"
+      click_button id: "#{@pet2.name} accept"
 
       expect(current_path).to eq("/admin/applications/#{@application.id}")
-      expect(page).to have_content("Pet denied for this Application")
+      expect(page).to have_content("Pet denied for this application.")
       expect(page).to_not have_button("Accept")
     end
   end
   it "buttons don't affect other applications" do
     visit "/admin/applications/#{@application.id}"
 
-    click_button("Accept")
+    click_button id: "#{@pet.name} accept"
 
     visit "/admin/applications/#{@application2.id}"
 
     expect(page).to have_button("Accept")
     expect(page).to have_button("Deny")
+  end
+  it "if all pets are approved, status is accepted" do
+    visit "/admin/applications/#{@application.id}"
+
+    click_button id: "#{@pet.name} accept"
+    click_button id: "#{@pet2.name} accept"
+    
+    expect(page).to have_content("Approved")
+  end
+  it "if all pets are accepted/rejected and one is rejected, status is rejected" do
+    visit "/admin/applications/#{@application.id}"
+
+    click_button id: "#{@pet.name} deny"
+    click_button id: "#{@pet2.name} accept"
+
+    expect(page).to have_content("Rejected")
+  end
+  it "if application is approved for a pet, that pet isn't adoptable" do
+    visit "/admin/applications/#{@application.id}"
+    click_button id: "#{@pet.name} accept"
+    click_button id: "#{@pet2.name} accept"
+    visit "/pets/#{@pet.id}"
+
+    expect(page).to have_content(false)
+  end
+  it 'shows that pet is approved for adoption if pet is on approved application' do
+    visit "/admin/applications/#{@application.id}"
+    click_button id: "#{@pet.name} accept"
+    click_button id: "#{@pet2.name} accept"
+    visit "/admin/applications/#{@application2.id}"
+
+    expect(page).to have_content("Pet has been approved for adoption by another applicant.")
+    expect(page).to_not have_button("Accept")
   end
 end
